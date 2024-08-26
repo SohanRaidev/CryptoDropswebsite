c5a534c798aa21315a4f9330e098ed4ca82d8ad5
unknown <steampunkfiver@gmail.com>
2024-08-27 01:44:31 +0530

    vercel

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ def index():
 
 @app.route('/privacy-policy')
 def privacy_policy():
-    return render_template('privacy.html')
+    return render_template('privacy-policy.html')
 
 @app.route('/terms-of-service')
 def terms():
-    return render_template('terms.html')
+    return render_template('terms-of-service.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
